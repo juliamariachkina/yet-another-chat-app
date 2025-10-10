b13e19cd858b203f06b59f07cb1218294b988222
Julie Mariachkina <iuliia@level.works>
2025-10-10 16:04:27 +0200

    feat: add simple websocket gateway

---
+++ apps/yet-another-chat-app/src/app/chat/chat.gateway.ts
@@ -0,0 +1,34 @@
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  //@ts-expect-error assigned by nestjs
+  server: Server;
+
+  handleConnection(client: Socket) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() message: { senderId: string; text: string }) {
+    console.log('Received message:', message);
+    this.server.emit('message', message);
+  }
+}
